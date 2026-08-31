a52461dfedc74328fc9d338a6c041cd1ab7c5cbf
y3Enzo <enzochalimpus@outlook.com>
2026-08-30 22:29:53 -0300

    feat(cadastro): cria estrutura base do frontend

---
+++ atualizacao-cadastral/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;

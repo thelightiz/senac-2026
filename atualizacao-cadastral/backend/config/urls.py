695b25b84cdd70717d160ae83e59e33b7bfbfc70
y3Enzo <enzochalimpus@outlook.com>
2026-09-06 21:33:10 -0300

    feat(cadastro): configura url para a API de login

--- atualizacao-cadastral/backend/config/urls.py
+++ atualizacao-cadastral/backend/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as users_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/login', users_view.login)
 ]

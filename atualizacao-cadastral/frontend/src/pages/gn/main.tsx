5fa50501a4c060ab15d12ce48f3c631feeadeb28
y3Enzo <enzochalimpus@outlook.com>
2026-09-14 22:18:37 -0300

    feat(cadastro): inicia a construção de páginas do GN

---
+++ atualizacao-cadastral/frontend/src/pages/gn/main.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+
+export const GNMyRequests = () => {
+  const test = async () => {
+    try {
+      await api.get ('/gn/minhas-solicitacoes');
+      console.log('Tem permissão');
+    } catch (error: any) {
+        console.log('Status:', error.response?.status);
+        console.log('Dados do erro (Django):', error.response?.data);
+        console.log('Full error:', error);
+      }
+  };
+
+  useEffect(() => {
+    test();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-3xl text-center">Minhas solicitações</h1>
+      
+    </>
+  );
+};

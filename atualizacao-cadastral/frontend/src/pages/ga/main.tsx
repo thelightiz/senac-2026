2b1b5e38a47eb3f6d85c96e1a0598cf8d13a1556
y3Enzo <enzochalimpus@outlook.com>
2026-09-15 18:00:35 -0300

    feat(cadastro): inicia construção das páginas do GA

---
+++ atualizacao-cadastral/frontend/src/pages/ga/main.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { api } from "../../services/api"
+
+export const GARequestQueue = () => {
+    const test = async () => {
+        try {
+            await api.get ('/ga/fila-de-solicitacoes');
+            console.log('Tem permissão')
+        } catch (error: any) {
+            console.log('Sem permissão')
+        }
+    };
+
+    useEffect(() => {
+        test();
+    }, []);
+
+    return (
+        <h1 className="text-3xl text-center">Fila de Solicitações</h1>
+    );
+};
--- atualizacao-cadastral/frontend/src/routes/routes.tsx
+++ atualizacao-cadastral/frontend/src/routes/routes.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/login/main';
 import { Index } from '../pages/index/main';
 import { GNMyRequests } from '../pages/gn/main';
+import { GARequestQueue } from '../pages/ga/main';
 import { ProtectedRoute } from '../services/permissions';
 import { Error403Page } from '../pages/errors/403/main';
-import { Error401Page } from '../pages/errors/401/main';
 
 export const Rotas = () => {
     return (
@@ -15,7 +15,7 @@ export const Rotas = () => {
           <Route 
             path="gn/*" 
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole='GN'>
                 <Routes>
                   <Route path="minhas-solicitacoes" element={<GNMyRequests />} />
                 </Routes>
@@ -23,10 +23,18 @@ export const Rotas = () => {
             } 
           />
           <Route
+          path="ga/*"
+          element={
+            <ProtectedRoute requiredRole='GA'>
+              <Routes>
+                <Route path="fila-de-solicitacoes" element={<GARequestQueue />} />
+              </Routes>
+            </ProtectedRoute>
+          }></Route>
+          <Route
             path="erro/*"
             element={
               <Routes>
-                <Route path="401" element={<Error401Page />} />
                 <Route path="403" element={<Error403Page />} />
               </Routes>
             }

bdade91b20a108d0f7bc994a0668c0ad92ab0b25
y3Enzo <enzochalimpus@outlook.com>
2026-09-14 22:14:58 -0300

    feat(cadastro): adiciona páginas de erro 401 e 403

---
+++ atualizacao-cadastral/frontend/src/pages/errors/401/main.tsx
@@ -0,0 +1,10 @@
+export const Error401Page = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-3xl">Erro 401</h1>
+        <h2 className="">Você tentou acessar uma página sem autenticar-se antes</h2>
+      </div>
+    </>
+  );
+};

a73f4ebeab685fef9217f4d10772a5fee6190d5c
y3Enzo <enzochalimpus@outlook.com>
2026-09-21 21:27:06 -0300

    refactor(cadastro): adiciona uma / no final de cada rota

--- atualizacao-cadastral/frontend/src/routes/routes.tsx
+++ atualizacao-cadastral/frontend/src/routes/routes.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Login } from "../pages/login/main";
 import { Index } from "../pages/index/main";
-import { GNMyRequests } from "../pages/gn/main";
+import { GNIndexPage} from "../pages/gn/index";
+import { GNCreateRequest } from "../pages/gn/createRequest";
 import { GARequestQueue } from "../pages/ga/main";
 import { ProtectedRoute } from "../services/permissions";
 import { Error403Page } from "../pages/errors/403/main";
@@ -11,13 +12,14 @@ export const Rotas = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index/>} />
-          <Route path="entrar" element={<Login/>} />
+          <Route path="entrar/" element={<Login/>} />
           <Route 
             path="gn/*" 
             element={
               <ProtectedRoute requiredRole="GN">
                 <Routes>
-                  <Route path="minhas-solicitacoes" element={<GNMyRequests />} />
+                  <Route path="minhas-solicitacoes/" element={<GNIndexPage/>} />
+                  <Route path="criar-solicitacao/" element={<GNCreateRequest/>} />
                 </Routes>
               </ProtectedRoute>
             } 
@@ -27,7 +29,7 @@ export const Rotas = () => {
           element={
             <ProtectedRoute requiredRole="GA">
               <Routes>
-                <Route path="fila-de-solicitacoes" element={<GARequestQueue />} />
+                <Route path="fila-de-solicitacoes/" element={<GARequestQueue />} />
               </Routes>
             </ProtectedRoute>
           }></Route>

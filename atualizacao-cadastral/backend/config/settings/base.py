600c256d565c0de6e98b4fb1a389a050ed664ba0
y3Enzo <enzochalimpus@outlook.com>
2026-09-23 17:50:51 -0300

    feat(cadastro): configura o projeto para guardar documentos

--- atualizacao-cadastral/backend/config/settings/base.py
+++ atualizacao-cadastral/backend/config/settings/base.py
@@ -176,6 +176,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
--- atualizacao-cadastral/backend/config/urls.py
+++ atualizacao-cadastral/backend/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
@@ -25,4 +27,4 @@ urlpatterns = [
     path('api/auth', users_views.AuthView.as_view()),
     path('api/gn/minhas-solicitacoes', requests_views.GNMyRequestsView.as_view()),
     path('api/gn/criar-solicitacao', requests_views.GNCreateRequestView.as_view())
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
